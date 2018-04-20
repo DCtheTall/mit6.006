99818f33b2b8599796158210af9473219ba34db4
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-04-20 01:10:31 -0400

    wikipedia scraper for document distance

---
+++ lecture2.py
@@ -0,0 +1,40 @@
+from urllib2 import urlopen
+from urllib import urlencode
+import json
+from bs4 import BeautifulSoup
+
+# Wikipedia scraper for getting documents to compare
+
+def get_article_w_prompt(prompt):
+  """
+  Prompts user for search query then scrapes
+  Wikipedia for an article with that query
+
+  Returns the content of the article as a string
+  """
+  query = raw_input(prompt)
+  url = 'https://en.wikipedia.org' \
+    '/w/api.php?action=query&list=search&format=json&%s' \
+      % urlencode({'srsearch': query})
+  json_res = urlopen(url).read()
+  data = json.loads(json_res)
+  results = data['query']['search']
+  if not results:
+    print 'Unable to find an article with that query. Please try again.\n'
+    return get_article_w_prompt(prompt)
+  title = '_'.join(results[0]['title'].split(' '))
+  url = 'https://en.wikipedia.org/wiki/%s' % title
+  html = urlopen(url).read()
+  parsed_html = BeautifulSoup(html, 'html.parser')
+  return parsed_html.body.find('div', attrs={'id': 'bodyContent'}).text
+
+
+def main():
+  first_doc = get_article_w_prompt(
+    'What would you like to search for the first article? '
+  )
+  second_doc = get_article_w_prompt(
+    'What would you like to search for the second article? '
+  )
+
+main()
