27327b8d5c7526f59931e5815f1d2e1b09eded59
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-05-23 01:37:33 -0400

    Djikstras algorithm

--- lecture15.py
+++ lecture15.py
@@ -30,7 +30,7 @@ def shortest_paths_in_dag(adjacency_list, weights, s):
   works
 
   """
-  sorted_dag = topological_sort(adjacency_list)
+  sorted_dag = topological_sort(adjacency_list) # topologically sort the DAG
   path_costs = {}
   predecessors = {}
   for v in sorted_dag: # first mark each node as having an infinite cost to reach
@@ -43,5 +43,33 @@ def shortest_paths_in_dag(adjacency_list, weights, s):
     for v in adjacency_list[u]:
       if path_costs[u] + weights[(u, v)] < path_costs[v]:  # relaxation occurs here
         path_costs[v] = weights[(u, v)]
+        predecessors[v] = u
+  return (path_costs, predecessors)
+
+
+def dijkstra(adjacency_list, weights, s):
+  """
+  Dijkstra's algorithm for finding
+  the least cost path to each vertex
+  from s given that each edge weight
+  is >= 0
+
+  """
+  unfinished = dict(adjacency_list)
+  unfinished[s] = 0
+  for u in unfinished: # create a dict of all unfinished vertices cost from s
+    if u != s:
+      unfinished[u] = float('inf')
+  path_costs = {}
+  predecessors = { s: None }
+  while unfinished:
+    u = min(unfinished) # get the closest vertex to s (starting with s)
+    path_costs[u] = unfinished[u]  # record the cost to get there
+    del unfinished[u] # remove it from the set of unfinished vertices
+    for v in adjacency_list[u]:
+      if v in path_costs: # for each unfinished vertex adjacent to u
+        continue
+      if path_costs[u] + weights[(u, v)] < unfinished[v]: # relax each path
+        unfinished[v] = path_costs[u] + weights[(u, v)]
         predecessors[v] = u
   return (path_costs, predecessors)
