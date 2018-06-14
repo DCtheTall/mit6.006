58434a094b215a867c7ea54b80f2c17c1feef174
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-06-13 22:10:06 -0400

    piano fingering

--- lecture22.py
+++ lecture22.py
@@ -2,7 +2,7 @@
 Dynamic Programming IV
 ======================
 
-1. Piano Fingers
+Piano Fingering
 
 Given a set of rules which determine the cost of
 certain piano transactions, what is the optimal
@@ -24,6 +24,7 @@ STRETCH_TABLE = {  # source: http://www.diva-portal.org/smash/get/diva2:768564/F
   (4, 5): (1, 1, 1, 2, 3, 6),
 }
 
+
 def get_key_color(key):
   """
   Get the color of the piano key
@@ -32,7 +33,8 @@ def get_key_color(key):
   """
   return 'black' if (key % 12) in (1, 3, 6, 8, 10) else 'white'
 
-def get_transition_cost(note1, note2, finger1, finger2):
+
+def get_transition_cost(note1, note2, f, g):
   """
   Get the cost of moving from note1 to note2 (integers)
   when finger1 is already playing note1
@@ -44,11 +46,15 @@ def get_transition_cost(note1, note2, finger1, finger2):
   did not in the interest of time.
 
   """
+  finger1 = min(f, g)
+  finger2 = max(f, g)
   transition = note2 - note1
   stretches = STRETCH_TABLE[(finger1, finger2)]
   cost = 0
-  if transition < stretches[0] or transition > stretches[5]:
-    return float('inf')
+  if transition < stretches[0]:
+    cost += 3 * (stretches[0] - transition)
+  if transition > stretches[5]:
+    cost += 3 * (transition - stretches[5])
   if transition < stretches[1]:
     cost += 2 * (stretches[1] - transition)
   if transition > stretches[4]:
@@ -58,8 +64,6 @@ def get_transition_cost(note1, note2, finger1, finger2):
   if transition > stretches[3]:
     cost += (1 if finger1 == 1 else 2) * (transition - stretches[3])
   if finger1 in (4, 5):
-    cost += 1
-  if finger2 in (4, 5):
     cost += 1
   if len({finger1, finger2}.intersection({3, 4, 5})) == 2:
     cost += 1
@@ -79,5 +83,43 @@ def get_optimal_fingering(song):
   Get the optimal fingering for a song of single
   notes represented by a list of keys
 
+  Given the cost model (which admittedly is probably not great)
+  this will find the optimal fingering. It's assumed that
+  playing the same note twice with the same finger has zero
+  cost
+
+  Complexity: O(n)
+
   """
-  pass
+  optimal_costs = {}
+  optimal_fingering = {}
+  n = len(song)
+  for i in range(n - 1, -1, -1):
+    for f in range(1, 6):
+      if i == n - 1:
+        optimal_costs[(i, f)] = 1 if f in (4, 5) else 0
+        optimal_fingering[(i, f)] = (f,)
+      else:
+        costs = {}
+        for g in range(1, 6):
+          if f == g:
+            if song[i] == song[i + 1]:
+              costs[0] = g
+              continue
+            else:
+              continue
+          cost = get_transition_cost(song[i], song[i + 1], f, g) \
+            + optimal_costs[(i + 1, g)]
+          if cost not in costs:
+            costs[cost] = g
+        best_cost = min(costs)
+        optimal_costs[(i, f)] = best_cost
+        next_finger = costs[best_cost]
+        optimal_fingering[(i, f)] = (f,) + optimal_fingering[(i + 1, next_finger)]
+  starting_finger = None
+  min_cost = float('inf')
+  for f in range(1, 6):
+    if optimal_costs[(0, f)] < min_cost:
+      starting_finger = f
+      min_cost = optimal_costs[(0, f)]
+  return (optimal_costs[(0, starting_finger)], optimal_fingering[(0, starting_finger)])
